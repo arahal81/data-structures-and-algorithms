d7452c3d8616eaf15f1dc3640b3e3c763635ebc5
Ali Rahhal <arahal81@gmail.com>
2021-07-29 22:12:12 +0300

    code challenge 05

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,55 @@
+class Node:
+    def __init__(self, value=""):
+        self.value = value
+        self.next = None
+
+    def __add__(self, other):
+        return Node(self.value + other.value)
+
+    def __str__(self):
+        return str(self.value)
+
+
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def includes(self, value):
+        is_last = False
+        current = self.head
+        print(current)
+        while not is_last:
+            if str(current) == str(value):
+                return True
+
+            if current.next == None:
+                is_last = True
+            current = current.next
+        return False
+
+    def insert(self, value):
+        node = Node(value)
+
+        if self.head:
+            node.next = self.head
+        self.head = node
+
+    def __str__(self) -> str:
+        string = ""
+        current = self.head
+
+        while current:
+            string += f" {'{'}{str(current.value)}{'}'} -> "
+            current = current.next
+        string += "NULL"
+        return string
+
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert(5)
+    ll.insert(4)
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = node1 + node2
+    print(ll.includes(5))

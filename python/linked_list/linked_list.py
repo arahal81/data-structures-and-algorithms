714eae94861daaf0d795b47107209a2bbd3157cc
Ali Rahhal <arahal81@gmail.com>
2021-08-12 09:51:19 +0300

    code challenge07 done

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -33,57 +33,68 @@ class LinkedList:
         if self.head:
             node.next = self.head
         self.head = node
+
     def append(self, value):
         node = Node(value)
-        if self.head==None:
-            self.head=node
+        if self.head == None:
+            self.head = node
         else:
             current_node = self.head
             while current_node.next:
                 current_node = current_node.next
             current_node.next = node
-    def insert_before(self,value,new_value):
+
+    def insert_before(self, value, new_value):
         node = Node(new_value)
-        current_node=self.head
-        if current_node.value==value:
-             node.next = self.head
-             self.head = node
+        current_node = self.head
+        if current_node.value == value:
+            node.next = self.head
+            self.head = node
         else:
             while current_node.next:
-                if current_node.next.value==value:
-                    node.next=current_node.next
-                    current_node.next=node
+                if current_node.next.value == value:
+                    node.next = current_node.next
+                    current_node.next = node
                     break
-                current_node=current_node.next
+                current_node = current_node.next
 
-
-    def insert_after(self,value,new_value):
+    def insert_after(self, value, new_value):
         node = Node(new_value)
-        current_node=self.head
+        current_node = self.head
         while current_node:
-            if current_node.value==value:
-                temp=current_node.next
-                current_node.next=node
-                node.next=temp
+            if current_node.value == value:
+                temp = current_node.next
+                current_node.next = node
+                node.next = temp
                 break
-            current_node=current_node.next
+            current_node = current_node.next
 
-    def kthFromEnd(self,k):
+    def kthFromEnd(self, k):
         current = self.head
         if current == None:
             return ("Empty List")
 
         if k <= -1:
-           return("Negative number not acceptable")
-        values=[]
-        while current:
-            values =values+ [current.value]
-            current = current.next
-        print(values)
-        try:
+            return("Negative number not acceptable")
+        # values=[]
+        # while current:
+        #     values =values+ [current.value]
+        #     current = current.next
+        # print(values)
+        # try:
 
-            return values[::-1][k]
-        except IndexError:
+        #     return values[::-1][k]
+        idx = -1
+        current = self.head
+        while current:
+            current = current.next
+            idx = idx + 1
+        if idx >= k:
+            current = self.head
+            for i in range(idx - k):
+                current = current.next
+            return current.value
+        else:
             return ("Index not found")
 
     def __str__(self) -> str:
@@ -96,14 +107,11 @@ class LinkedList:
         string += "NULL"
         return string
 
+
 if __name__ == "__main__":
     ll = LinkedList()
     ll.insert(5)
     ll.insert(4)
     ll.insert(1)
     ll.insert(2)
-    node1 = Node(1)
-    node2 = Node(2)
-    node3 = node1 + node2
-    print(ll.includes(5))
-    print(ll.kthFromEnd(6))
+    print(ll.kthFromEnd(3))
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -38,62 +38,75 @@ def llt():
     linked_list.insert(10)
     linked_list.insert(5)
     return linked_list
+
+
 def test_linked_append(llt):
     llt.append(6)
     assert str(llt) == f' {{5}} ->  {{10}} ->  {{15}} ->  {{6}} -> NULL'
 
 # Can successfully add multiple nodes to the end of a linked list
 
+
 def test_linked_multiple_append(llt):
     llt.append(4)
     llt.append(7)
     llt.append(11)
-    assert str(llt) == f' {{5}} ->  {{10}} ->  {{15}} ->  {{4}} ->  {{7}} ->  {{11}} -> NULL'
+    assert str(
+        llt) == f' {{5}} ->  {{10}} ->  {{15}} ->  {{4}} ->  {{7}} ->  {{11}} -> NULL'
 
 # Can successfully insert a node before a node located in the middle of a linked list
 
+
 def test_linked_insert_before(llt):
-    llt.insert_before(5,3)
+    llt.insert_before(5, 3)
     assert str(llt) == f' {{3}} ->  {{5}} ->  {{10}} ->  {{15}} -> NULL'
 
 # Can successfully insert a node before the first node of a linked list
 
+
 def test_linked_insert_before_first(llt):
-    llt.insert_before(15,7)
+    llt.insert_before(15, 7)
     assert str(llt) == f' {{5}} ->  {{10}} ->  {{7}} ->  {{15}} -> NULL'
 
 # Can successfully insert after a node in the middle of the linked list
 
+
 def test_linked_insert_after(llt):
-    llt.insert_after(10,8)
+    llt.insert_after(10, 8)
     assert str(llt) == f' {{5}} ->  {{10}} ->  {{8}} ->  {{15}} -> NULL'
 
 # Can successfully insert a node after the last node of the linked list
 
+
 def test_linked_insert_after_last(llt):
-    llt.insert_after(15,20)
+    llt.insert_after(15, 20)
     assert str(llt) == f' {{5}} ->  {{10}} ->  {{15}} ->  {{20}} -> NULL'
+
+
 def test_linked_values(llt):
     assert str(llt) == f" {{5}} ->  {{10}} ->  {{15}} -> NULL"
 
 # this tests for negative input and indax not exist and happy path
+
+
 @pytest.mark.parametrize(
     "input,expected_value",
     [
-        (-5,"Negative number not acceptable"),
-        (10,"Index not found"),
-        (0,15),
-        (1,10),
-        (2,5),
+        (-5, "Negative number not acceptable"),
+        (10, "Index not found"),
+        (0, 15),
+        (1, 10),
+        (2, 5),
     ],
 )
-def test_all_valid_dice_rolls(input, expected_value,llt):
+def test_kth_from_end(input, expected_value, llt):
 
     output = llt.kthFromEnd(input)
-    print(output)
     assert output == expected_value
 
 # this tests for empty linked list
+
+
 def test_kthFromEnd_of_empty_linkedList():
-    empty_ll=LinkedList()
+    empty_ll = LinkedList()
     assert empty_ll.kthFromEnd(1) == "Empty List"

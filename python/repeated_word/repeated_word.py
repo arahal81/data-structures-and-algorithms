d2a308a980b63233a1ebdb7f38e2ab45d1add34a
Ali Rahhal <arahal81@gmail.com>
2021-09-05 23:04:13 +0300

    code challenge 31

---
+++ python/repeated_word/repeated_word.py
@@ -0,0 +1,19 @@
+from hash_map.hash_map import Hashmap
+import re
+
+
+def repeated_word(text: str) -> str:
+    hashmap = Hashmap()
+    words = re.findall(r'\w+', text)
+    for word in words:
+        new_word = word.upper()
+        if hashmap.contains(new_word):
+            return new_word
+        else:
+            hashmap.add(new_word)
+
+    return "There are no repetition"
+
+
+if __name__ == "__main__":
+    print(repeated_word("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."))
